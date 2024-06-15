0a51053fcaf958c7f30a7dfdcc18208527c33495
Michael Volfson <volfson.mihael@gmail.com>
2024-06-15 09:00:29 +0300

    7

--- app/src/main/java/com/example/profisrael/Registration.java
+++ app/src/main/java/com/example/profisrael/Registration.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -14,205 +16,98 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class Registration extends AppCompatActivity {
 
-
+    private EditText login_reg_editText, pass_reg_editText, tel_reg_editText, whatsapp_reg_editText, instagram_reg_editText, name_of_your_service_reg_editText, from_ils_reg_editText, to_ils_reg_editText, address_reg_editText, description_reg_editText;
+    private Spinner spinner_city_spinner, spinner_category_spinner, spinner_service_spinner;
 
     ImageView btnPupMenu;
     Button loginButton;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        Spinner spinner_city = findViewById(R.id.spinner_city);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.spinner_city_items, android.R.layout.simple_spinner_item);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_city.setAdapter(adapter);
+        // Initialize Spinners
+        spinner_city_spinner = findViewById(R.id.spinner_city);
+        ArrayAdapter<CharSequence> adapter_city = ArrayAdapter.createFromResource(this, R.array.spinner_city_items, android.R.layout.simple_spinner_item);
+        adapter_city.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_city_spinner.setAdapter(adapter_city);
+        spinner_city_spinner.setSelection(0, false);
 
-        Spinner spinner_category = findViewById(R.id.spinner_category);
-        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
-                R.array.spinner_category_items, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_category.setAdapter(adapter1);
+        spinner_category_spinner = findViewById(R.id.spinner_category);
+        ArrayAdapter<CharSequence> adapter_category = ArrayAdapter.createFromResource(this, R.array.spinner_category_items, android.R.layout.simple_spinner_item);
+        adapter_category.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_category_spinner.setAdapter(adapter_category);
+        spinner_category_spinner.setSelection(0, false);
 
-        Spinner spinner_service = findViewById(R.id.spinner_service);
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
-                R.array.spinner_service_items, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_service.setAdapter(adapter2);
+        spinner_service_spinner = findViewById(R.id.spinner_service);
+        ArrayAdapter<CharSequence> adapter_service = ArrayAdapter.createFromResource(this, R.array.spinner_service_items, android.R.layout.simple_spinner_item);
+        adapter_service.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_service_spinner.setAdapter(adapter_service);
+        spinner_service_spinner.setSelection(0, false);
+
+        // Initialize EditTexts
+        login_reg_editText = findViewById(R.id.login_reg);
+        pass_reg_editText = findViewById(R.id.pass_reg);
+        tel_reg_editText = findViewById(R.id.tel_reg);
+        whatsapp_reg_editText = findViewById(R.id.whatsapp_reg);
+        instagram_reg_editText = findViewById(R.id.instagram_reg);
+        name_of_your_service_reg_editText = findViewById(R.id.name_of_your_service_reg);
+        from_ils_reg_editText = findViewById(R.id.from_ils_reg);
+        to_ils_reg_editText = findViewById(R.id.to_ils_reg);
+        address_reg_editText = findViewById(R.id.address_reg);
+        description_reg_editText = findViewById(R.id.description_reg);
+
+        // Set focus listeners for EditTexts
+        setFocusListener(login_reg_editText);
+        setFocusListener(pass_reg_editText);
+        setFocusListener(tel_reg_editText);
+        setFocusListener(whatsapp_reg_editText);
+        setFocusListener(instagram_reg_editText);
+        setFocusListener(name_of_your_service_reg_editText);
+        setFocusListener(from_ils_reg_editText);
+        setFocusListener(to_ils_reg_editText);
+        setFocusListener(address_reg_editText);
+        setFocusListener(description_reg_editText);
+
+        // Set focus and item listeners for Spinners
+        setSpinnerListener(spinner_city_spinner);
+        setSpinnerListener(spinner_category_spinner);
+        setSpinnerListener(spinner_service_spinner);
 
 
 
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        EditText passEditText = findViewById(R.id.pass_reg);
 
-        EditText descriptionEditText = findViewById(R.id.description_reg);
-
-        descriptionEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        // Устанавливаем слушатель для CheckBox
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    descriptionEditText.setBackgroundResource(R.drawable.rounded_edit_focused);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // Показываем пароль
+                    passEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
                 } else {
-                    descriptionEditText.setBackgroundResource(R.drawable.rounded_edit);
+                    // Скрываем пароль
+                    passEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                 }
+
+                // Перемещаем курсор в конец текста
+                passEditText.setSelection(passEditText.getText().length());
             }
         });
-
-        EditText login_regEditText = findViewById(R.id.login_reg);
-
-        login_regEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    login_regEditText.setBackgroundResource(R.drawable.rounded_edit_focused);
-                } else {
-                    login_regEditText.setBackgroundResource(R.drawable.rounded_edit);
-                }
-            }
-        });
-
-        EditText pass_reg_EditText = findViewById(R.id.pass_reg);
-
-        pass_reg_EditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    pass_reg_EditText.setBackgroundResource(R.drawable.rounded_edit_focused);
-                } else {
-                    pass_reg_EditText.setBackgroundResource(R.drawable.rounded_edit);
-                }
-            }
-        });
-
-//        Spinner spinner_city_Spinner = findViewById(R.id.spinner_city);
-//
-//        spinner_city_Spinner.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//            @Override
-//            public void onFocusChange(View v, boolean hasFocus) {
-//                if (hasFocus) {
-//                    spinner_city_Spinner.setBackgroundResource(R.drawable.rounded_edit_focused);
-//                } else {
-//                    spinner_city_Spinner.setBackgroundResource(R.drawable.rounded_edit);
-//                }
-//            }
-//        });
-
-//        Spinner spinner_category_Spinner = findViewById(R.id.spinner_category);
-//
-//        spinner_category_Spinner.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//            @Override
-//            public void onFocusChange(View v, boolean hasFocus) {
-//                if (hasFocus) {
-//                    spinner_category_Spinner.setBackgroundResource(R.drawable.rounded_edit_focused);
-//                } else {
-//                    spinner_category_Spinner.setBackgroundResource(R.drawable.rounded_edit);
-//                }
-//            }
-//        });
-
-
-//        Spinner spinner_service1 = findViewById(R.id.spinner_service);
-
-
-        EditText tel_regEditText = findViewById(R.id.tel_reg);
-
-        tel_regEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    tel_regEditText.setBackgroundResource(R.drawable.rounded_edit_focused);
-                } else {
-                    tel_regEditText.setBackgroundResource(R.drawable.rounded_edit);
-                }
-            }
-        });
-
-        EditText whatsapp_reg_EditText = findViewById(R.id.whatsapp_reg);
-
-        whatsapp_reg_EditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    whatsapp_reg_EditText.setBackgroundResource(R.drawable.rounded_edit_focused);
-                } else {
-                    whatsapp_reg_EditText.setBackgroundResource(R.drawable.rounded_edit);
-                }
-            }
-        });
-
-        EditText instagram_reg_EditText = findViewById(R.id.instagram_reg);
-
-        instagram_reg_EditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    instagram_reg_EditText.setBackgroundResource(R.drawable.rounded_edit_focused);
-                } else {
-                    instagram_reg_EditText.setBackgroundResource(R.drawable.rounded_edit);
-                }
-            }
-        });
-
-
-        EditText name_of_your_service_reg_EditText = findViewById(R.id.name_of_your_service_reg);
-
-        name_of_your_service_reg_EditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    name_of_your_service_reg_EditText.setBackgroundResource(R.drawable.rounded_edit_focused);
-                } else {
-                    name_of_your_service_reg_EditText.setBackgroundResource(R.drawable.rounded_edit);
-                }
-            }
-        });
-
-        EditText from_ils_reg_EditText = findViewById(R.id.from_ils_reg);
-
-        from_ils_reg_EditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    from_ils_reg_EditText.setBackgroundResource(R.drawable.rounded_edit_focused);
-                } else {
-                    from_ils_reg_EditText.setBackgroundResource(R.drawable.rounded_edit);
-                }
-            }
-        });
-
-        EditText to_ils_reg_EditText = findViewById(R.id.to_ils_reg);
-
-        to_ils_reg_EditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    to_ils_reg_EditText.setBackgroundResource(R.drawable.rounded_edit_focused);
-                } else {
-                    to_ils_reg_EditText.setBackgroundResource(R.drawable.rounded_edit);
-                }
-            }
-        });
-
-        EditText address_reg_EditText = findViewById(R.id.address_reg);
-
-        address_reg_EditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    address_reg_EditText.setBackgroundResource(R.drawable.rounded_edit_focused);
-                } else {
-                    address_reg_EditText.setBackgroundResource(R.drawable.rounded_edit);
-                }
-            }
-        });
-
-
 
         btnPupMenu = findViewById(R.id.menuPUP);
         registerForContextMenu(btnPupMenu);
@@ -224,7 +119,6 @@ public class Registration extends AppCompatActivity {
         });
 
         loginButton = findViewById(R.id.loginButton);
-
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -232,19 +126,21 @@ public class Registration extends AppCompatActivity {
             }
         });
     }
+
     private void loginClick() {
         Intent intent = new Intent(this, EnterActivity.class);
         startActivity(intent);
     }
 
-    public void logoClick(View v){
+    public void logoClick(View v) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater menuInflater =  new MenuInflater(this);
+        MenuInflater menuInflater = new MenuInflater(this);
         menuInflater.inflate(R.menu.menu_items, menu);
     }
 
@@ -279,5 +175,67 @@ public class Registration extends AppCompatActivity {
             intent.setPackage(null);
             startActivity(intent);
         }
+    }
+
+    private void setFocusListener(final EditText editText) {
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    highlightView(editText);
+                } else {
+                    removeHighlight(editText);
+                }
+            }
+        });
+    }
+
+    private void setSpinnerListener(final Spinner spinner) {
+        spinner.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    highlightView(spinner);
+                } else {
+                    removeHighlight(spinner);
+                }
+            }
+        });
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                highlightView(spinner);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                removeHighlight(spinner);
+            }
+        });
+    }
+
+    private void highlightView(View view) {
+        view.setBackgroundResource(R.drawable.rounded_edit_focused);
+        removeHighlightFromOthers(view);
+    }
+
+    private void removeHighlight(View view) {
+        view.setBackgroundResource(R.drawable.rounded_edit);
+    }
+
+    private void removeHighlightFromOthers(View currentView) {
+        if (currentView != login_reg_editText) removeHighlight(login_reg_editText);
+        if (currentView != pass_reg_editText) removeHighlight(pass_reg_editText);
+        if (currentView != tel_reg_editText) removeHighlight(tel_reg_editText);
+        if (currentView != whatsapp_reg_editText) removeHighlight(whatsapp_reg_editText);
+        if (currentView != instagram_reg_editText) removeHighlight(instagram_reg_editText);
+        if (currentView != name_of_your_service_reg_editText) removeHighlight(name_of_your_service_reg_editText);
+        if (currentView != from_ils_reg_editText) removeHighlight(from_ils_reg_editText);
+        if (currentView != to_ils_reg_editText) removeHighlight(to_ils_reg_editText);
+        if (currentView != address_reg_editText) removeHighlight(address_reg_editText);
+        if (currentView != description_reg_editText) removeHighlight(description_reg_editText);
+        if (currentView != spinner_city_spinner) removeHighlight(spinner_city_spinner);
+        if (currentView != spinner_category_spinner) removeHighlight(spinner_category_spinner);
+        if (currentView != spinner_service_spinner) removeHighlight(spinner_service_spinner);
     }
 }
